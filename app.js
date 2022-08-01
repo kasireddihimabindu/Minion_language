5a5939f0850a3bd247ae2fb4310df8c6bea02055
kasireddihimabindu <kasireddi.himabindu@gmail.com>
2022-08-01 11:10:14 +0530

    feat :Minion

---
+++ app.js
@@ -0,0 +1,7 @@
+var name= prompt("What's your name ?")
+ var welcomeMessage= "Hello!! " + name;
+ alert(welcomeMessage);  
+
+var button= document.querySelector(".translate")
+button.onclick= function(){   
+}
