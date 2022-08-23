abc42af9b773814f18ddb66c6fa6de6b6de7a798
kasireddihimabindu <kasireddi.himabindu@gmail.com>
2022-08-23 23:05:54 +0530

    Take input and console

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var name= prompt("What's your name ?")
+var name= prompt("What's your name ?");
  var welcomeMessage= "Hello!! " + name;
  alert(welcomeMessage);  
 
-var button= document.querySelector(".translate")
-button.onclick= function(){   
+var translate_button= document.querySelector(".translate")
+var input_text = document.querySelector("#input_text")
+
+
+function clickEventHandler(){
+    console.log("Clicked")
+    console.log("Input - ",input_text.value)
 }
+translate_button.addEventListener("click",clickEventHandler);
