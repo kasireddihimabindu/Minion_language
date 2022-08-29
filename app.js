861f5672be2cc0aff5e8e91500db2a6a94485012
kasireddihimabindu <kasireddi.himabindu@gmail.com>
2022-08-29 19:19:28 +0530

    Banana talks
    Done changes

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var name= prompt("What's your name ?");
  var welcomeMessage= "Hello!! " + name;
  alert(welcomeMessage);  
 
-var translate_button= document.querySelector(".translate")
-var input_text = document.querySelector("#input_text")
-
+var translate_button= document.querySelector(".translate");
+var input_text = document.querySelector("#input_text");
+var output_text = document.querySelector("#output");
 
 function clickEventHandler(){
-    console.log("Clicked")
-    console.log("Input - ",input_text.value)
-}
+    console.log(output_text);
+    document.querySelector('#output').innerText = "agyudhkjdh" + input_text.value;
+};
+
 translate_button.addEventListener("click",clickEventHandler);
